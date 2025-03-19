a5cf012570cc2e7eacefe0937d54caa39a876d8f
Yngve Heggelund <ynhe@norceresearch.no>
2025-03-19 09:33:34 +0100

    Initial script for modelling herring

--- src/prol_spheroid_vectorized.py
+++ src/prol_spheroid_vectorized.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 import csv
 from scipy.special import lpmn
-
+import platform
 
 from funcs.FUNC_read_fortran_output_vectorized import func_Rad_SWF_ArgExp, func_dr_values_ArgExp, \
     func_Smn_eta_c_from_dr_ArgExp, Multi_ArgExp, Sum_ArgExp, Get_ArgExp, func_Integrate_SmnEta1_SmlEta2_ArgExp, \
@@ -16,6 +16,7 @@ from funcs.FUNC_read_fortran_output_vectorized import func_Rad_SWF_ArgExp, func_
 # The spherical Hankel function of the first kind h1_n(z) is defined by:
 #    h1_n(z)=j_n(z)+i*y_n(z)
 # where j_n(z) and y_n(z) are the spherical Bessel functions of the first and second kinds.
+
 
 
 class ProlateSpheroid:
@@ -40,6 +41,8 @@ class ProlateSpheroid:
 
         self.solver = solver
         self.precision_fbs = settings.precision_fbs
+        system = platform.system()
+        self.python_exe = 'python' if system == 'Windows' else 'python3'
 
 
     def run(self, ts_file_name):
@@ -126,7 +129,7 @@ class ProlateSpheroid:
         script_path1 = os.path.join(ParentDIR, 'src', 'Run_param_fort_FromPython.py')
         print(script_path1)
         # Run Python Script:
-        subprocess.run(['python3', script_path1], capture_output=True, text=True)
+        subprocess.run([self.python_exe, script_path1], capture_output=True, text=True)
         #>>>>>>>>>
         #|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
         #|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -174,7 +177,7 @@ class ProlateSpheroid:
             
             #print(script_path)
             # Run Fortran:
-            subprocess.run(['python3', script_path], capture_output=True, text=True)
+            subprocess.run([self.python_exe, script_path], capture_output=True, text=True)
             #>>>>>>>>>
 
             content_Rad_SWF_hs=infunc_content_Rad_SWF(ParentDIR)
@@ -191,7 +194,7 @@ class ProlateSpheroid:
 
 
             # Run Fortran:
-            subprocess.run(['python3', script_path], capture_output=True, text=True)
+            subprocess.run([self.python_exe, script_path], capture_output=True, text=True)
             #>>>>>>>>>
             content_Rad_SWF_hw=infunc_content_Rad_SWF(ParentDIR)
             #    content_Ang_SWF_hw=infunc_content_Ang_SWF()
@@ -273,7 +276,7 @@ class ProlateSpheroid:
         script_path1 = ParentDIR+'/src/'+'Run_param_fort_FromPython.py'
         print(script_path1)
         # Run Python Script:
-        subprocess.run(['python3', script_path1], capture_output=True, text=True)
+        subprocess.run([self.python_exe, script_path1], capture_output=True, text=True)
 
         f = freq_Hz
 
@@ -309,7 +312,7 @@ class ProlateSpheroid:
         script_path = ParentDIR+'/src/'+'Run_profcn_II_fort_FromPython.py'
         print(script_path)
         # Run Fortran:
-        subprocess.run(['python3', script_path], capture_output=True, text=True)
+        subprocess.run([self.python_exe, script_path], capture_output=True, text=True)
         #>>>>>>>>>
 
         content_Rad_SWF_hs=infunc_content_Rad_SWF(ParentDIR)
@@ -323,7 +326,7 @@ class ProlateSpheroid:
         write_Inputfile(c_nondimensional,self.AspRatio,M_order_Fortran,N_order_Fortran,Theta_IncDeg, ParentDIR)
 
         # Run Fortran:
-        subprocess.run(['python3', script_path], capture_output=True, text=True)
+        subprocess.run([self.python_exe, script_path], capture_output=True, text=True)
         #>>>>>>>>>
         content_Rad_SWF_hw=infunc_content_Rad_SWF(ParentDIR)
         #    content_Ang_SWF_hw=infunc_content_Ang_SWF()
---
+++ src/script_herring.py
@@ -0,0 +1,80 @@
+from prol_spheroid_vectorized import ProlateSpheroid
+from IterativeSolvers import BiCGSTABSolver, IterativeRefinement
+import os
+
+class HerringSettings:
+
+    def __init__(self, prefix, ro_s, c_s, a, b, delta_f, min_freq, max_freq, theta_i_deg):
+        self.prefix = prefix        # just a name to identify the output files
+        # media properties
+        self.ro_w = 1027            # the density of the surrounding media [kg/m^3]
+        self.ro_s = ro_s            # the density of the spheroid [kg/m^3]
+        self.c_w = 1500             # the sound speed in the surrounding media [m/s]
+        self.c_s = c_s             # the sound speed in the spheroid [m/s]
+
+        # geometrical properties
+        self.a = a               # the length of the semi-major axis [m]
+        self.b = b              # the length of the semi-minor axis [m]
+
+        # frequencies
+        self.delta_f = delta_f         # the distance between frequencies [Hz]
+        self.min_freq = min_freq       # the start frequency [Hz]
+        self.max_freq = max_freq       # the end frequency [Hz]
+
+        #incident angle
+        self.theta_i_deg = theta_i_deg       # the incidence angle [degrees]
+
+        # precision
+        self.precision_fbs = 1e-6
+
+
+def createSettings(fish_length, depth, incident_angle):
+    a = fish_length * 0.26 * 0.5
+    b_0 = 0.01 * 0.5 # from Gorska & Ona - todo: maybe it also should be varied with length?
+    b = b_0 * (1 + depth/10)**(-0.5)
+
+    ro_w = 1027              # density of water
+    # adjust ro_s to be approx 14 kg/m^3 at 100 m depth
+    ro_s_0 = 0.00129 * ro_w  # density at 0m
+    ro_s_100 = 14            # density at 100m
+    ro_s = ro_s_0 * (1 + depth * (ro_s_100 - ro_s_0) / (ro_s_0 * 100))
+    c_w = 1500               # sound speed in water
+    c_s = 0.23 * c_w         # the sound speed is assumed not to depend on depth
+    return HerringSettings('herring', ro_s, c_s, a, b, 1000, 1000, 260000, incident_angle)
+
+
+if __name__ == '__main__':
+    # if the path to the gfortran compiler is not already in the PATH environment variable, it can be added here
+    os.environ['PATH'] += os.pathsep + os.path.abspath(r'C:\bin\mingw64\bin')
+
+    fish_lengths = [0.15, 0.25]
+    incidence_angles = [90, 82, 75]
+    depths = [10, 50, 100]
+
+    for incident_angle in incidence_angles:
+        for fish_length in fish_lengths:
+            for depth in depths:
+                print('Running with length {}, incident angle {}, and depth {}'.format(fish_length, incident_angle, depth))
+                herring_settings = createSettings(fish_length, depth, incident_angle)
+                #solver = BiCGSTABSolver('ILU')
+                solver = IterativeRefinement('LU')
+                ts_file_name = 'ts_vs_freq_loop_{}_a_{}_b_{}_f1_{}_f2_{}_rhos_{:.2f}_IncAngle_{}_depth_{}_length_{}_{}.csv'.format(herring_settings.prefix,
+                                                                                                                                   herring_settings.a, herring_settings.b,
+                                                                                                                                   int(herring_settings.min_freq / 1000),
+                                                                                                                                   int(herring_settings.max_freq / 1000),
+                                                                                                                                   herring_settings.ro_s, herring_settings.theta_i_deg,
+                                                                                                                                   depth, fish_length,
+                                                                                                                                   solver.solver_name())
+
+                ParentDIR=os.path.split(os.getcwd())[0]
+                freq_resp_file = os.path.join(ParentDIR, 'temp', ts_file_name)
+                if os.path.isfile(freq_resp_file):
+                    print('skipping {} since it already exists'.format(ts_file_name))
+                    continue
+                spheroid = ProlateSpheroid(herring_settings, solver)
+                try:
+                    spheroid.run(ts_file_name)
+                except Exception as e:
+                    # skip to the next case if a simulation fails for any reason
+                    print(e)
+                    pass
